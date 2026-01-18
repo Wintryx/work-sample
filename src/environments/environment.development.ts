a8a422a8fc29928cf164bb64f8bef2da48a41a77
ArneWinter <info@wintryx.de>
2026-01-18 18:28:46 +0100

    environment configurations

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: "/api",
+  oidc: {
+    issuer: "https://fake-idp.example",
+    audience: "epm-progress-maker",
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: "/api",
+  oidc: {
+    issuer: "https://auth.epm-progress-maker.com",
+    audience: "epm-progress-maker",
+  }
+};

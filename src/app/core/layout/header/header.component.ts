0419d7188ee203b99f5963b8b1aa185efbe6d0d5
ArneWinter <info@wintryx.de>
2026-01-21 11:17:23 +0100

    more epm classes for header and page components

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,31 +1,31 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from "@angular/core";
-import { AuthFacade } from "@core/auth";
-import { MatIcon } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
-import { RouterLink, RouterLinkActive } from "@angular/router";
-import { MatButton } from "@angular/material/button";
-import { DateFormatPipe } from "@shared/pipes/date-format-pipe";
+import {ChangeDetectionStrategy, Component, DestroyRef, inject, signal} from "@angular/core";
+import {AuthFacade} from "@core/auth";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {DateFormatPipe} from "@shared/pipes/date-format-pipe";
 
 @Component({
-  selector: "app-header-component",
-  imports: [MatIcon, MatMenuModule, RouterLink, RouterLinkActive, MatButton, DateFormatPipe],
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.scss",
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: "app-header-component",
+    imports: [MatIcon, MatMenuModule, RouterLink, RouterLinkActive, MatButton, DateFormatPipe, MatIconButton],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  // Using the facade to drive the UI state
-  protected readonly authFacade = inject(AuthFacade);
-  protected readonly currentTime = signal(new Date());
-  private readonly timer = setInterval(() => this.currentTime.set(new Date()), 1000 * 60); // Update every minute
+    // Using the facade to drive the UI state
+    protected readonly authFacade = inject(AuthFacade);
+    protected readonly currentTime = signal(new Date());
+    private readonly timer = setInterval(() => this.currentTime.set(new Date()), 1000 * 60); // Update every minute
 
-  constructor() {
-    // Cleanup timer to prevent memory leaks
-    inject(DestroyRef).onDestroy(() => clearInterval(this.timer));
-  }
+    constructor() {
+        // Cleanup timer to prevent memory leaks
+        inject(DestroyRef).onDestroy(() => clearInterval(this.timer));
+    }
 
-  protected onLogout(): void {
-    this.authFacade.logout();
-  }
+    protected onLogout(): void {
+        this.authFacade.logout();
+    }
 }
